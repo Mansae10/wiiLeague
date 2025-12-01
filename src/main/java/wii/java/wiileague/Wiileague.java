9448f2ad85488244c384d819a2efffedbb30c38b
Justin Hong <JustHong8032@gmail.com>
2025-12-01 13:32:20 -0500

    Initial commit: Set up Spring Boot + MongoDB Atlas with secure configuration.

---
+++ src/main/java/wii/java/wiileague/Wiileague.java
@@ -0,0 +1,13 @@
+package wii.java.wiileague;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiileagueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WiileagueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wii/java/wiileague/WiileagueApplicationTests.java
@@ -0,0 +1,13 @@
+package wii.java.wiileague;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WiileagueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
